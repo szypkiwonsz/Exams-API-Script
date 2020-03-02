38ffed66a520b07e54aa227f664b3f09c4f942cf
szypkiwonsz <szypkiwonsz@github.com>
2020-03-02 11:12:11 +0100

    Three commands have been added

--- script.py
+++ script.py
@@ -1,28 +1,86 @@
 import sys
 
 from api import Api
+from calculation import Calculation
 
 args = sys.argv
-args = args[1:]
 
 
 class Script:
 
-    def __init__(self, command):
+    def __init__(self, command, first_arg, second_arg, third_arg, fourth_arg):
 
         self.command = command
+        self.first_arg = first_arg
+        self.second_arg = second_arg
+        self.third_arg = third_arg
+        self.fourth_arg = fourth_arg
 
-    if len(args) == 0:
-        print("This command not exist! Write: python main.py --help")
+    def empty_command(self):
+        if len(self.command) == 0:
+            print("This command not exist! Write: python main.py --help")
 
-    else:
-        if sys.argv[1] == "--get_data":
+    def get_data(self):
+        if self.first_arg == "--get_data":
             api = Api("https://api.dane.gov.pl/resources/17363/data", "exams_data.sqlite")
-            if api.is_not_empty():
-                print("Database is already filled with data!")
+            if api.is_empty():
+                api.get_insert_data()
             else:
-                api.get_data()
+                print("Database is already filled with data!")
+
+    def first(self):
+        if self.first_arg == "--1":
+            calculation = Calculation("exams_data.sqlite", self.second_arg, self.third_arg, self.fourth_arg)
+            calculation.if_gender()
+            calculation.joined()
+            calculation.sum()
+            calculation.average()
+            calculation.print_result()
+
+    def second(self):
+        if self.first_arg == "--2":
+            calculation = Calculation("exams_data.sqlite", self.second_arg, self.third_arg, self.fourth_arg)
+            calculation.if_gender()
+            calculation.joined()
+            calculation.passed()
+            calculation.sum()
+            calculation.percent()
+            calculation.print_result()
+
+    def third(self):
+        if self.first_arg == "--3":
+            calculation = Calculation("exams_data.sqlite", self.second_arg, self.third_arg, self.fourth_arg)
+            calculation.if_gender()
+            calculation.highest_pass()
+            calculation.print_result()
 
 
 if __name__ == '__main__':
-    Script(sys.argv[1])
+
+    if args[1:]:
+        first_argument = args[1]
+    else:
+        first_argument = ""
+
+    if args[2:]:
+        second_argument = args[2]
+    else:
+        second_argument = ""
+
+    if args[3:]:
+        third_argument = args[3]
+    else:
+        third_argument = ""
+
+    if args[4:]:
+        fourth_argument = args[4]
+    else:
+        fourth_argument = ""
+
+    script = Script(args[1:], first_argument, second_argument, third_argument, fourth_argument)
+
+    script.empty_command()
+    script.get_data()
+    script.first()
+    script.second()
+    script.third()
