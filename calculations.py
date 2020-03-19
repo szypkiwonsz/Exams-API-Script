0bf1b7da27468b698a18842ed27a0cbc08ceb999
szypkiwonsz <szypkiwonsz@github.com>
2020-03-19 16:32:38 +0100

    Code improvement

--- calculations.py
+++ calculations.py
@@ -3,81 +3,75 @@ import sys
 from database import Database
 
 
-class Calculation(Database):
+class Calculations(Database):
 
-    def __init__(self, name, territory=None, year=None, gender=""):
-
+    def __init__(self, name, territory=None, year=None, gender="", territory_second=None):
         super().__init__(name)
         self.territory = territory
-        self.territory_second = None
+        self.territory_second = territory_second
         self.year = year
         self.gender = gender
+
+        self.people_joined = 0
+        self.people_passed = 0
         self.data = None
-        self.data_second = None
-        self.joined_people = 0
-        self.passed_people = 0
         self.counted_years = 0
+        self.data_second = None
 
-    def print_result(self):
-
-        print("{} - {}".format(self.year, self.data))
-
-    def if_gender(self):
-
-        if self.gender != "":
+    def check_gender(self):
+        if self.gender.lower() == "kobiety" or self.gender.lower() == "mężczyźni":
             self.gender = "AND GENDER = '{}'".format(self.gender)
+        else:
+            self.gender = ""
+            print("Gender entered incorrectly.")
+
+    def joined(self):
+        self.query("SELECT PEOPLE FROM EXAM_DATA WHERE TERRITORY = '{}' AND YEAR <= '{}' AND JOINED_AND_PASSED = "
+                   "'przystąpiło' {}".format(self.territory, self.year, self.gender))
+        self.people_joined = sum([item[0] for item in self.cursor.fetchall()])
+
+    def passed(self):
+        self.query("SELECT PEOPLE FROM EXAM_DATA WHERE TERRITORY = '{}' AND YEAR <= '{}' AND JOINED_AND_PASSED = "
+                   "'zdało' {}".format(self.territory, self.year, self.gender))
+        self.people_passed = sum([item[0] for item in self.cursor.fetchall()])
+
+    def highest_pass(self):
+        self.query("SELECT TERRITORY, MAX(PEOPLE) FROM EXAM_DATA WHERE YEAR <= '{}' AND JOINED_AND_PASSED = "
+                   "'zdało' AND TERRITORY != 'Polska' {}".format(self.year, self.gender))
+        self.data = [item[0] for item in self.cursor.fetchall()]
+        self.data = self.data[0]
 
     def count_years(self):
-
         try:
-            self.counted_years = int(self.year[-1]) + 1
-
+            return int(self.year[-1]) + 1
         except ValueError as e:
             sys.exit("Third argument is invalid!")
 
     def average(self):
+        self.data = self.data // self.count_years()
 
-        self.data = self.data // self.counted_years
+    def print(self):
+        print("{} - {}".format(self.year, self.data))
+
+    def sum(self):
+        self.data = self.people_joined + self.people_passed
 
     def add_percent(self):
         self.data = str(self.data)
         self.data += "%"
 
-    def percent(self):
+    def percent_passed(self):
+        self.data = (self.people_passed * 100) // self.data
 
-        self.data = (self.passed_people * 100) // self.data
-
-    def sum(self):
-
-        self.data = self.joined_people + self.passed_people
+    def get_value_for_regression(self):
+        self.query("SELECT TERRITORY, PEOPLE, YEAR FROM EXAM_DATA WHERE JOINED_AND_PASSED = 'zdało' {}".format
+                   (self.gender))
+        self.data = self.cursor.fetchall()
 
     @staticmethod
     def get_cleaned_value_from_list(data):
         data = data[0]
         return data
-
-    def joined(self):
-
-        self.query("SELECT PEOPLE FROM EXAM_DATA WHERE TERRITORY = '{}' AND YEAR <= '{}' AND JOINED_AND_PASSED = "
-                   "'przystąpiło' {}".format(self.territory, self.year, self.gender))
-        self.joined_people = sum([item[0] for item in self.cursor.fetchall()])
-
-    def passed(self):
-
-        self.query("SELECT PEOPLE FROM EXAM_DATA WHERE TERRITORY = '{}' AND YEAR <= '{}' AND JOINED_AND_PASSED = "
-                   "'zdało' {}".format(self.territory, self.year, self.gender))
-        self.passed_people = sum([item[0] for item in self.cursor.fetchall()])
-
-    def highest_pass(self):
-
-        self.query("SELECT TERRITORY, MAX(PEOPLE) FROM EXAM_DATA WHERE YEAR <= '{}' AND JOINED_AND_PASSED = "
-                   "'zdało' AND TERRITORY != 'Polska' {}".format(self.year, self.gender))
-        self.data = [item[0] for item in self.cursor.fetchall()]
-
-    def get_value_for_regression(self):
-
-        self.query("SELECT TERRITORY, PEOPLE, YEAR FROM EXAM_DATA WHERE JOINED_AND_PASSED = 'zdało' {}".format(self.gender))
-        self.data = self.cursor.fetchall()
 
     def count_years_from_database(self):
 
@@ -86,7 +80,7 @@ class Calculation(Database):
         self.year = self.cursor.fetchall()
         self.year = self.get_cleaned_value_from_list(self.year)
         self.year = str(self.get_cleaned_value_from_list(self.year))
-        self.count_years()
+        self.counted_years = self.count_years()
 
     @staticmethod
     def print_regression(territory, year_first, year_second):
--- script.py
+++ script.py
@@ -1,153 +1,135 @@
 import sys
 
 from api import Api
-from calculation import Calculation
-
+from calculations import Calculations
 args = sys.argv
 
 
-class Script:
+class Arguments:
+    def __init__(self, arguments):
 
-    def __init__(self, command, first_arg, second_arg, third_arg, fourth_arg):
+        self.arguments = arguments
+        self.first = ""
+        self.second = ""
+        self.third = ""
+        self.fourth = ""
 
-        self.command = command
-        self.first_arg = first_arg
-        self.second_arg = second_arg
-        self.third_arg = third_arg
-        self.fourth_arg = fourth_arg
+    def get_arguments(self):
+        if len(self.arguments) == 1:
+            self.first = self.arguments[0]
 
-    @staticmethod
-    def check_gender(argument):
-        if argument == "kobiety" or argument == "mężczyźni" or argument == "":
-            return True
+        elif len(self.arguments) == 2:
+            self.first = self.arguments[0]
+            self.second = self.arguments[1]
+
+        elif len(self.arguments) == 3:
+            self.first = self.arguments[0]
+            self.second = self.arguments[1]
+            self.third = self.arguments[2]
+
         else:
-            return False
+            self.first = self.arguments[0]
+            self.second = self.arguments[1]
+            self.third = self.arguments[2]
+            self.fourth = self.arguments[3]
 
-    @staticmethod
-    def check_year(argument):
-        try:
-            if 2009 < int(argument) < 2019:
-                return True
-            else:
-                return False
-        except ValueError as e:
-            sys.exit("Arguments are invalid!")
+    def first_command(self):
+        if self.first == "--1" and self.second != "" and self.third != "":
+            return True
 
-    def empty_command(self):
-        if len(self.command) == 0:
-            print("This command not exist! Write: python script.py --help")
+    def second_command(self):
+        if self.first == "--2" and self.second != "" and self.third != "":
+            return True
+
+    def third_command(self):
+        if self.first == "--3" and self.second != "":
+            return True
+
+    def fourth_command(self):
+        if self.first == "--4":
+            return True
+
+    def fifth_command(self):
+        if self.first == "--5" and self.second != "" and self.third != "":
+            return True
+
+    def help_command(self):
+        if self.first == "--help":
+            return True
 
     def get_data(self):
-        if self.first_arg == "--get_data":
-            api = Api("https://api.dane.gov.pl/resources/17363/data", "exams_data.sqlite")
-            if api.is_empty():
-                api.get_insert_data()
-            else:
-                print("Database is already filled with data!")
-
-    def first(self):
-        if self.first_arg == "--1" and self.second_arg and self.third_arg and self.check_gender(self.fourth_arg) and self.check_year(self.third_arg):
-            calculation = Calculation("exams_data.sqlite", self.second_arg, self.third_arg, self.fourth_arg)
-            calculation.if_gender()
-            calculation.joined()
-            calculation.sum()
-            calculation.count_years()
-            calculation.average()
-            calculation.print_result()
-        else:
-            pass
-
-    def second(self):
-        if self.first_arg == "--2" and self.second_arg and self.third_arg and self.check_gender(self.fourth_arg) and self.check_year(self.third_arg):
-            calculation = Calculation("exams_data.sqlite", self.second_arg, self.third_arg, self.fourth_arg)
-            calculation.if_gender()
-            calculation.joined()
-            calculation.passed()
-            calculation.sum()
-            calculation.percent()
-            calculation.add_percent()
-            calculation.print_result()
-        else:
-            pass
-
-    def third(self):
-        if self.first_arg == "--3" and self.second_arg and self.check_gender(self.fourth_arg) and self.check_year(self.second_arg):
-            calculation = Calculation("exams_data.sqlite", None, self.second_arg, self.third_arg)
-            calculation.if_gender()
-            calculation.highest_pass()
-            calculation.data = calculation.get_cleaned_value_from_list(calculation.data)
-            calculation.print_result()
-        else:
-            pass
-
-    def fourth(self):
-        if self.first_arg == "--4":
-            calculation = Calculation("exams_data.sqlite")
-            calculation.gender = self.second_arg
-            calculation.if_gender()
-            calculation.get_value_for_regression()
-            calculation.count_years_from_database()
-            calculation.regression()
-        else:
-            pass
-
-    def fifth(self):
-        if self.first_arg == "--5" and self.second_arg and self.third_arg and self.check_gender(self.fourth_arg):
-            calculation = Calculation("exams_data.sqlite", self.second_arg)
-            calculation.territory_second = self.third_arg
-            calculation.gender = self.fourth_arg
-            calculation.if_gender()
-            calculation.get_value_for_compare()
-            calculation.compare()
-        elif self.first_arg == "--help":
-            print('Commands:')
-            print('REMEMBER: FIRST YOU SHOULD FILL THE DATABASE WITH DATA. -> "--get_data"')
-            print('Choosing to divide results by gender is not required but it is possible by adding the last command '
-                  'as "kobiety" or "mężczyźni".')
-            print(' --help -> show this basic help menu.')
-            print(' --get_data -> Importing data from api to database.')
-            print(' --1 TERRITORY YEAR -> displaying the average number of people who took the exam for a given '
-                  'province over the years, up to the given year.')
-            print(' --2 TERRITORY YEAR -> displaying the percentage pass rate for a given province over the years, e.g.')
-            print(' --3 YEAR -> Providing the voivodship with the best pass rate in a given year.')
-            print(' --4 -> Displaying voivodships that recorded a decrease in the success rate in the following year.')
-            print(' --5 FIRST_TERRITORY SECOND_TERRITORY -> comparison of two voivodships - for the two voivodships '
-                  'listed, listing which voivodship had better pass rates in each available year.')
-
-    def check_command(self):
-        if self.first_arg != "1" or self.first_arg != "2" or self.first_arg != "3" or self.first_arg != "4" \
-             "" or self.first_arg != "5" or self.first_arg != "--help" or self.first_arg != "--get_data":
+        if self.first == "--get_data":
             return True
 
 
 if __name__ == '__main__':
 
-    if args[1:]:
-        first_argument = args[1]
+    argument = Arguments(args[1:])
+    argument.get_arguments()
+
+    if argument.first_command():
+        first_command = Calculations("exams_data.sqlite", argument.second, argument.third, argument.fourth)
+        first_command.check_gender()
+        first_command.joined()
+        first_command.sum()
+        first_command.average()
+        first_command.print()
+        first_command.close()
+
+    elif argument.second_command():
+        second_command = Calculations("exams_data.sqlite", argument.second, argument.third, argument.fourth)
+        second_command.check_gender()
+        second_command.joined()
+        second_command.passed()
+        second_command.sum()
+        second_command.percent_passed()
+        second_command.add_percent()
+        second_command.print()
+        second_command.close()
+
+    elif argument.third_command():
+        third_command = Calculations("exams_data.sqlite", None, argument.second, argument.third)
+        third_command.check_gender()
+        third_command.highest_pass()
+        third_command.print()
+        third_command.close()
+
+    elif argument.fourth_command():
+        fourth_command = Calculations("exams_data.sqlite", None, None, argument.third)
+        fourth_command.check_gender()
+        fourth_command.get_value_for_regression()
+        fourth_command.count_years_from_database()
+        fourth_command.regression()
+        fourth_command.close()
+
+    elif argument.fifth_command():
+        fifth_command = Calculations("exams_data.sqlite", argument.second, None, argument.fourth, argument.third)
+        fifth_command.check_gender()
+        fifth_command.get_value_for_compare()
+        fifth_command.compare()
+        fifth_command.close()
+
+    elif argument.get_data():
+        api = Api("https://api.dane.gov.pl/resources/17363/data", "exams_data.sqlite")
+        if api.is_empty():
+            api.get_insert_data()
+        else:
+            print("Database is already filled with data!")
+
+    elif argument.help_command():
+        print('Commands:')
+        print('REMEMBER: FIRST YOU SHOULD FILL THE DATABASE WITH DATA. -> "--get_data"')
+        print('Choosing to divide results by gender is not required but it is possible by adding the last command '
+              'as "kobiety" or "mężczyźni".')
+        print(' --help -> show this basic help menu.')
+        print(' --get_data -> Importing data from api to database.')
+        print(' --1 TERRITORY YEAR -> displaying the average number of people who took the exam for a given '
+              'province over the years, up to the given year.')
+        print(
+            ' --2 TERRITORY YEAR -> displaying the percentage pass rate for a given province over the years, e.g.')
+        print(' --3 YEAR -> Providing the voivodship with the best pass rate in a given year.')
+        print(' --4 -> Displaying voivodships that recorded a decrease in the success rate in the following year.')
+        print(' --5 FIRST_TERRITORY SECOND_TERRITORY -> comparison of two voivodships - for the two voivodships '
+              'listed, listing which voivodship had better pass rates in each available year.')
     else:
-        first_argument = ""
-
-    if args[2:]:
-        second_argument = args[2]
-    else:
-        second_argument = ""
-
-    if args[3:]:
-        third_argument = args[3]
-    else:
-        third_argument = ""
-
-    if args[4:]:
-        fourth_argument = args[4]
-    else:
-        fourth_argument = ""
-
-    script = Script(args[1:], first_argument, second_argument, third_argument, fourth_argument)
-
-    script.empty_command()
-    script.get_data()
-    script.first()
-    script.second()
-    script.third()
-    script.fourth()
-    script.fifth()
+        print("This command not exist! Write: python script.py --help")
